1c4c9dfe47c2c141330ffd45679ee4972cdbbb37
romisuman6201 <romisuman141@gmail.com>
2023-08-17 15:57:17 +0530

    Second commit

--- src/test/java/com/sample/Demo.java
+++ src/test/java/com/sample/Demo.java
@@ -3,11 +3,10 @@ package com.sample;
 public class Demo {
 	public void add() {
 		System.out.println("Addition of two numbers");
+		System.out.println("Addition of three number");
 	}
 
 	public static void main(String[] args) {
-		
-		
 
 	}
 
