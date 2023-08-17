f5749b0ee4b52236dd0935392a05ac8098208a04
romisuman6201 <romisuman141@gmail.com>
2023-08-17 15:53:14 +0530

    First message

---
+++ src/test/java/com/sample/Demo.java
@@ -0,0 +1,14 @@
+package com.sample;
+
+public class Demo {
+	public void add() {
+		System.out.println("Addition of two numbers");
+	}
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
