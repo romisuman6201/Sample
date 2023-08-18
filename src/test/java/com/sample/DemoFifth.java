1dc410776ef8252dd8249bc6cf17cf72c7f78b5c
romisuman6201 <romisuman141@gmail.com>
2023-08-18 14:54:33 +0530

    ok

---
+++ src/test/java/com/sample/DemoFifth.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+public class DemoFifth {
+
+	public void fifth() {
+		System.out.println("===Fourth Execution");
+	}
+
+	public static void main(String[] args) {
+		DemoFifth d = new DemoFifth();
+		d.fifth();
+
+	}
+
+}
